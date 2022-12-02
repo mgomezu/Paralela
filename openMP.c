e43c2d9d65ca1017f3430efbdc26b80ce5a85838
mgomezu <49196702+mgomezu@users.noreply.github.com>
2022-12-02 17:34:50 -0500

    Update openMP.c

--- openMP.c
+++ openMP.c
@@ -3,6 +3,7 @@
   
 // OpenMP header 
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,38 +13,23 @@
 /*****************************************************************************/
 void multMatrix(const int *A, const int *B, int *C, int numElements, int XDIM, int YDIM, int num_threads)
 {
-  int i, x;
-  int rowAshared[1024];
-
-
-  omp_set_num_threads(num_threads);
-  #pragma omp parallel
-  {
-    int id,nthrds;
-    double x;
-    id = omp_get_thread_num();
-    nthrds = omp_get_num_threads();  
-
-    for(i = id; i < XDIM; i=i+nthrds){
-      rowAshared[i] = *(A + i);
-    }
-  }
-
+  int i, x, j;
   
   omp_set_num_threads(num_threads);
   #pragma omp parallel
   {
-    int id,nthrds, j, x, pos;
+    int id,nthrds, pos;
     id = omp_get_thread_num();
     nthrds = omp_get_num_threads();  
-    for (int j = id; j<YDIM; j=j+nthrds){
+
+    #pragma omp for
+    for (int j = 0; j<YDIM; j++){
       for(x = 0; x < XDIM; x++)
       {   
-          
           pos=(XDIM*j)+x;
           *(C + pos) = 0;
           for(i = 0; i < XDIM; i++){
-              *(C + pos) = *(C + pos) + (rowAshared[i] * (*(B + (i*YDIM) + pos )));
+              *(C + pos) = *(C + pos) + (*(A + (x*XDIM) + i ) * (*(B + (i*YDIM) + x )));
           }
       }  
     }
@@ -77,34 +63,18 @@ int main(int argc, char *argv[])
     
     int N = atoi(argv[1]);
     int NUMTHREADS = atoi(argv[2]);
-    int BLOCKSPERGRID = N/NUMTHREADS;
     int XDIM = N;
     int YDIM = N;
     int MATRIXSIZE = XDIM*YDIM;
     
     int i, v=0;
-    int blocksPerGrid, threadsPerBlock;
     int numElements = MATRIXSIZE;
     size_t size = MATRIXSIZE * sizeof(int);
     if(v == 1) printf("[Matrix mult of %d elements]\n", numElements);
 
-    // Allocate the host input vector A
     int *h_A = (int *)malloc(size);
-
-    // Allocate the host input vector B
     int *h_B = (int *)malloc(size);
-
-    // Allocate the host output vector C
     int *h_C = (int *)malloc(size);
-
-    // Verify that allocations succeeded
-    if (h_A == NULL || h_B == NULL || h_C == NULL)
-    {
-        fprintf(stderr, "Failed to allocate host vectors!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Initialize the host input vectors
     
     for(i = 0; i < MATRIXSIZE; i++){
         *(h_A + i) = rand() & 0xF;
@@ -112,7 +82,6 @@ int main(int argc, char *argv[])
         *(h_C + i) = 0;
     }
     
-
     multMatrix(h_A, h_B, h_C, numElements, XDIM, YDIM, NUMTHREADS);
 
     printMatrix(h_C, XDIM, YDIM);
